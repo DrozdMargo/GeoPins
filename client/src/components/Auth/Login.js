e1157fb96f56316f59cfe44ad6ad2a661dcb651d
margo drozd <margodrozd.1@gmail.com>
2019-07-16 17:02:51 +0300

    added aoth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+require('dotenv').config();
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    
+  };
+  
+  return (
+    <GoogleLogin clientId="484748405974-drdqqdbioov1q721q3dmeurfmbqdq0nb.apps.googleusercontent.com"
+                 onSuccess={onSuccess}
+                 isSignedIn={true}
+    />
+)
 };
 
 const styles = {
