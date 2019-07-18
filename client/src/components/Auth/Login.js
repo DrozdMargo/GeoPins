4fc1946f83fb35078eedc8f41c631dd602162138
margo drozd <margodrozd.1@gmail.com>
2019-07-18 10:45:02 +0300

    added authenticated user from apollo server

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 require('dotenv').config();
 
+const ME_QUERY = `{
+  me {
+    _id
+    name
+    email
+  }
+}`;
+
 const Login = ({ classes }) => {
-  const onSuccess = googleUser => {
+  const onSuccess = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token;
-    
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    });
+    const data = await client.request(ME_QUERY);
+    console.log(data);
   };
   
   return (
--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,22 @@
+const { AuthenticationError } = require('apollo-server');
+
 const user = {
   _id: '1',
   name: 'Margo',
-  email: '44fff@email.com',
+  email: 'margodrozd.1@gmail.com',
   picture: 'https://cloudnary/',
+};
+
+const authenticated = next => (root, args, ctx, info) => {
+  if(!ctx.currentUser) {
+    throw new AuthenticationError('You must be logged in');
+  }
+  return next(root, args, ctx, info);
 };
 
 module.exports = {
   
   Query: {
-    me: () => user
+    me: authenticated((root, args, ctx, info) => ctx.currentUser)
   }
 };
